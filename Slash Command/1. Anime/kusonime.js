56595caeb2b33470106b9f814ea0b6010ed46d62
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-23 19:15:28 +0700

    Create kusonime.js

---
+++ Slash Command/1. Anime/kusonime.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+module.exports.name = 'kusonime'
+module.exports.cooldown = 60
+module.exports.options = [{
+  name: 'query',
+  description: 'anime yang mau dicari',
+  type: 3,
+  required: true
+}]
+module.exports.run = async function (client, passedOptions, interaction) => {
+  let query = passedOptions[0].value.split(' ').join('-')
+  let search = await axios.get(`http://posantai.bugs.today/kusonime/api/anime/${query}`)
+  search = search.data
+  let sinopsis = search.sinopsis.length > 150 ? search.sinopsis.slice(0, 150) + '...' : search.sinopsis
+  
+  let oriPlaceHolder = []
+  
+  for(const resolusi of search.list_download[0][1]) {
+    /*[
+       {resolusi: ..., link: [ {platform: ..., link: ...}, {...} ] },
+       {resolusi: ..., link: [ {platform: ..., link: ...}, {...} ] }
+      ]
+    */
+   let data = {name: `**${resolusi.resolusi}**`, value: resolusi.map(a => `[${a.platform}](${a.link})`).join('\n'), inline: true}
+   oriPlaceHolder.push(data)
+  }
+  
+  interaction.reply({embeds: [{title: search.title,
+                              description: sinopsis,
+                               color: 'RANDOM',
+                               fields: [{name: 'Genre', value: search.genre.map(g => g.name).join(', '), inline: false},
+                                        {name: 'Type', value: search.type, inline: true},
+                                        {name: 'Status', value: search.status, inline: true},
+                                        {name: 'Rating', value: search.score, inline: true},
+                                        {name: 'Durasi', value: search.durasi, inlime: true},
+                                        {name: 'Tanggal Rilis', value: search.release, inline: true}
+                                        ],
+                              thumbnail: search.thumbnail}, {
+                                title: 'link download',
+                                color: 'RANDOM',
+                                fields: oriPlaceHolder 
+                              }]
+                   })
+}
