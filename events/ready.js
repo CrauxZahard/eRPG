17cc7b04a9a89c28288728c5f7021dda9fb83732
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-23 11:59:12 +0700

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = async client => {
+  await require('../handler/slashCommand.js')(client); //bulkOverwrite-ing slash command
+  console.log(`${client.user.tag} is ready to launch!`)
+}
