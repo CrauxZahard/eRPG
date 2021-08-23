3dcacd01c1b207cbbe95fc85c303d5f687d3f81f
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-23 17:34:45 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,9 +1,46 @@
 module.exports = (client, interaction) => {
+  
   if(!interaction.inGuild()) {
     return interaction.reply({content: 'Hello there! this command can only be used in a guild (server) :D'})
   }
+  
   if(interaction.isCommand()) {
     let command = client.slashCommand.get(interaction.commandName)
-    command.run(client, interaction.options.data)
+    
+    try {
+      command.run(client, interaction.options.data)
+    }
+    catch (err) {
+      interaction.reply('uh oh, there is an error executing the command.')
+      console.log(err)
+    }
+    
+  }
+  
+  if(interaction.isButton()) {
+    //customId should look like 'commandName_{user.id}'
+    let command = client.buttonCommad.get(interaction.customId.split('_')[0])
+    
+    try {
+      command.run(client, interaction.customId)
+    }
+    catch (err) {
+      interaction.reply('there was an error executing that command! pls try again later.')
+      console.log(err)
+    }
+    
+  }
+  
+  if(interaction.isSelectMenu()) {
+    let command = client.selectMenu.get(interaction.customId.split('_')[0])
+    
+    try {
+      command.run(client, interaction.customId)
+    }
+    catch (err) {
+      interaction.reply('uhh.. the button won\'t work for some reason. weird.')
+      console.log(err)
+    }
+    
   }
 }
