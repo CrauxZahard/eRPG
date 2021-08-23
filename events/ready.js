0b42d13bc2bebd54876c487c0553650e42b18b68
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-23 19:25:37 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = async client => {
-  await require('../handler/slashCommand.js')(client); //bulkOverwrite-ing slash command
+  await require('../handler/SlashCommand.js')(client); //bulkOverwrite-ing slash command
   console.log(`${client.user.tag} is ready to launch!`)
 }
