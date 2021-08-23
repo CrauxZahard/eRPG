e486c8bf2061a4bafc05be3249586bfa828b6d7b
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-23 11:50:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const Discord = require('discord.js')
+const {Client, Intents} = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ Intents: [ Intents.FLAGS.GUILDS ] });
+client.db = require('quick.db');
+
+require('./handler/events.js')(client);
+
+client.login(token);
