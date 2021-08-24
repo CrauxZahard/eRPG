ce365dacc9c4b336782d7a79d9b91022991ec346
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-24 10:17:41 +0700

    Update SlashCommand.js

--- handler/SlashCommand.js
+++ handler/SlashCommand.js
@@ -4,14 +4,15 @@ const { Collection } = require('discord.js')
 module.exports = client => {
   const slash = new SlashCommand(client)
   client.slashCommand = new Collection()
-  
+  let overwriteCommandData = []
   let mainFolder = fs.readdirSync('./Slash Commands/');
   for (const folder of mainFolder) {
     const files = fs.readdirSync(`./Slash Commands/${folder}/`)
     for (const file of files) {
       const command = require(`../Slash Commands/${folder}/${file}`)
-      slash.addCommand(command)
+      overwriteCommandData.push(command)
       client.slashCommand.set(command.name, command)
     }
   }
+  await slash.bulkOverwrite(overwriteCommandData)
 }
