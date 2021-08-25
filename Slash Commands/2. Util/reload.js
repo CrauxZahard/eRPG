69b3bd750d6e7630bdfcfd546de016910e0afff5
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-25 19:07:00 +0700

    Update reload.js

--- Slash Commands/2. Util/reload.js
+++ Slash Commands/2. Util/reload.js
@@ -23,8 +23,38 @@ module.exports.options = [{name: 'name',
                           }]
 
 module.exports.run = async (client, passedOptions, interaction) => {
-  let choice = passedOptions[1].value ?? 'slash'
+  let choice = passedOptions[1].value ?? 'slashCommand'
+  const path = choice == 'slashCommand' ? '../../Slash Commands/' : choice == 'buttonCommand' ? '../../Button Commands/' : '../../Select Menus/'
+  
   if(passedOptions[0].value == 'all') {
-   c 
+    client[choice].clear()
+    client[choice] = new Collection()
+    const mainFolder = fs.readdirSync(path)
+    for(const folder of mainFolder) {
+      const files = fs.readdirSync(`${path}${folder}/`)
+      for(const file of files) {
+        const command = require(`${path}${folder}/${file}`)
+        client[choice].set(command.name, command)
+      }
+    }
+    return interaction.reply('success reloaded command.')
   }
+  
+  else {
+    const deleted = client[choice].delete(passedOptions[0].value)
+    if(!deleted) return interaction.reply(`command with name \`${passedOptions[0].value}\` doesn't exist`)
+    const mainFolder = fs.readdirSync(path)
+    for(const folder of mainFolder) {
+      const files = fs.readdirSync(`${path}${folder}/`)
+      for(const file of files) {
+        const command = require(`${path}${folder}/${file}`)
+        if(command.name == passedOptions[0].value) {
+          client[choice].set(command.name, command)
+          return interaction.reply('success reloaded that command. path: ' + path + folder + '/' + file)
+        }
+      }
+    }
+    return interaction.reply(`command with name \`passedOptions[0].value\` not found. it is deleted instead.`)
+  }
+  
 }
