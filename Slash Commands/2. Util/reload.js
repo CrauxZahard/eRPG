1a5a0d3b4dfa85cbc56ebd7998319607d6eb3208
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-25 18:33:33 +0700

    Update reload.js

--- Slash Commands/2. Util/reload.js
+++ Slash Commands/2. Util/reload.js
@@ -12,56 +12,19 @@ module.exports.options = [{name: 'name',
                           type: 3,
                           choices: [{
                              name: 'Slash Command',
-                             value: 'slash'
+                             value: 'slashCommand'
                             }, {
                              name: 'Button',
-                             value: 'button'
+                             value: 'buttonCommand'
                             }, {
                              name: 'Select Menu',
-                             value: 'select'
+                             value: 'selectCommand'
                             }]
                           }]
 
 module.exports.run = async (client, passedOptions, interaction) => {
-  let choice = passedOptions.find(x => x.name == 'type').value ?? 'slash'
-  let commandName = passedOptions.find(x => x.name == 'name').value
-  
-  if(choice == 'slash') {
-    if(commandName == 'all') {
-      client.slashCommand.clear()
-      client.slashCommand = new Collection()
-      let mainFolder = fs.readdirSync('./Slash Commands/');
-      for (const folder of mainFolder) {
-        const files = fs.readdirSync(`./Slash Commands/${folder}/`)
-        for (const file of files) {
-          const command = require(`../Slash Commands/${folder}/${file}`)
-          client.slashCommand.set(command.name, command)
-         }
-       }
-    }
-    
-    else {
-      let mainFolder = fs.readdirSync('./Slash Commands/');
-      for (const folder of mainFolder) {
-        const files = fs.readdirSync(`./Slash Commands/${folder}/`)
-        for (const file of files) {
-          const command = require(`../Slash Commands/${folder}/${file}`)
-          if(command.name == commandName) {
-            client.slashCommand.set(command.name, command)
-            return interaction.reply('success reloaded command. path: ' + `../Slash Commands/${folder}/${file}`)
-          }
-         }
-       }
-      return interaction.reply(':x: there is no command with that name.')
-    }
+  let choice = passedOptions[1].value ?? 'slash'
+  if(passedOptions[0].value == 'all') {
+   c 
   }
-  
-  else if(choice == 'button') {
-    
-  }
-  
-  else if(choice == 'select') {
-    
-  }
-  
 }
